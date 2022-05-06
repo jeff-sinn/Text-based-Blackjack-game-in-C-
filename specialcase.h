4959337b9ac93d45d63ba6b24e04d089dc129b0b
jeff-sinn <100022332+jeff-sinn@users.noreply.github.com>
2022-05-06 18:30:14 +0800

    Add files via upload
    header file of specialcase.cpp

---
+++ specialcase.h
@@ -0,0 +1,12 @@
+//specialcase.h
+
+#ifndef SPECIALCASE_H
+#define SPECIALCASE_H
+
+bool checkstraight(int x, int y, int z);
+bool checkflush(int x, int y, int z);
+bool checkthreeofakind(int x,int y,int z);
+bool checkpairs(int x,int y);
+bool checkpairsflush(int x,int y);
+
+#endif
